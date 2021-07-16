87df50dfd4776b70a3f9619a9b75fd0f15c299bd
喻杰 <yuj@golive.cc>
2021-07-16 16:31:19 +0800

    1.0 demo搭建

---
+++ my.js
@@ -0,0 +1,71 @@
+const myChart = echarts.init(document.getElementById('main'));
+//内容⬇️️️⬇️️️⬇️️️
+
+
+option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            crossStyle: {
+                color: '#999'
+            }
+        }
+    },
+    toolbox: {
+        feature: {
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+        }
+    },
+    legend: {
+        data: ['金额', '涨幅']
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisPointer: {
+                type: 'shadow'
+            }
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            min: 0,
+            max: 120,
+            axisLabel: {
+                formatter: '{value} K'
+            }
+        },
+        {
+            type: 'value',
+            name: '涨幅',
+            min: 0,
+            max: 30,
+            axisLabel: {
+                formatter: '{value} K'
+            }
+        }
+    ],
+    series: [
+
+        {
+            name: '金额',
+            type: 'line',
+            data: [33.11, 35.67, 44.04,58.36,64.93,65.29,73.86]
+        },{
+            name: '涨幅',
+            type: 'bar',
+            yAxisIndex: 1,
+            data: [0, 2.56, 8.37,14.32,6.57,0.36,8.57]
+        }
+    ]
+};
+
+//内容⬆️⬆️⬆️
+//使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
